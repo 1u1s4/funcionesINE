6ce9356f15d90729be246ac8aee4fda1032a6c67
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-07-08 11:24:30 -0500

    a

--- R/funcionesINE.R
+++ R/funcionesINE.R
@@ -443,8 +443,8 @@ etiquetasLineas <- function(graph, posiciones, precision=1)
     
     
     
-   
-    print(dato,i,tam = length(d$x))
+    print(d$x)
+    print(length(d$x))
     d$etiqueta <- formatC(as.numeric(completarEtiquetas(dato,i,tam = length(d$x))), format = 'f', big.mark = ',', digits = pkg.env$precision, drop0trailing = enteros)
     print("#####LAS ETIQUETAS SON ##########" )
     print(d$etiqueta)

e86edc81755ccd614e509cf24ad5d312446ad4f0
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-07-08 11:34:32 -0500

    Revert "a"
    This reverts commit 6ce9356f15d90729be246ac8aee4fda1032a6c67.

--- R/funcionesINE.R
+++ R/funcionesINE.R
@@ -443,8 +443,8 @@ etiquetasLineas <- function(graph, posiciones, precision=1)
     
     
     
-    print(d$x)
-    print(length(d$x))
+   
+    print(dato,i,tam = length(d$x))
     d$etiqueta <- formatC(as.numeric(completarEtiquetas(dato,i,tam = length(d$x))), format = 'f', big.mark = ',', digits = pkg.env$precision, drop0trailing = enteros)
     print("#####LAS ETIQUETAS SON ##########" )
     print(d$etiqueta)

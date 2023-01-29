dafad5100690493c761ebf4783c61712df2fe2ed
Hugo Garcia <hugoallangm@gmail.com>
2023-01-28 19:02:58 -0600

    Radar inicial hecho
    Radar inicial hecho

--- R/pruebas.R
+++ R/pruebas.R
@@ -4,8 +4,8 @@
 
 #Creando archivo de pruebas
 
-categorias <- c("Agua", "Saneamiento", "Electricidad", "Eliminación desechos")
-valores <- c(80, 15, 20, 30)
+categorias <- c("Agua", "Saneamiento", "Electricidad", "Eliminacion de desechos", 'Otro')
+valores <- c(80, 15, 20, 30, 50)
 
 testSpider <- data.frame(categorias, valores)
 colnames(testSpider) <- c("x","y")
@@ -13,10 +13,17 @@ colnames(testSpider) <- c("x","y")
 
 
 arreglar <- function(data){
-  dataT <- t(data)
-  dataTT <- rbind(max(rep(max(data$y)*1.25, 100), length(data$x)), rep(0, length(data$x)), dataT[2,] )
-  colnames(dataTT) <- data$x
-  return(as.data.frame(dataTT))
+  dataT <- as.data.frame(matrix(ncol = length(data$x)))
+  dataT[1,] <- data$y
+  colnames(dataT) <- data$x
+  dataTT <- rbind(rep(max(data$y*1.15,100), length(data$x)), rep(0, length(data$x)), dataT )
+  return(dataTT)
 }
 
+tt <- arreglar(testSpider)
+tt <- as.data.frame(tt)
+
+
+tikzDevice::tikz('../ENEI2022/Graficas/prueba.tex', standAlone = F, bg = "transparent" , width = 4 , height= 2.75, sanitize= F)
 fmsb::radarchart(tt)
+dev.off()

e0324d5526bc76812b0b68cd5ed73d82823a38f1
Hugo Garcia <hugoallangm@gmail.com>
2023-01-30 13:12:53 -0600

    Arreglando funciones

--- R/pruebas.R
+++ R/pruebas.R
@@ -12,20 +12,11 @@ colnames(testSpider) <- c("x","y")
 
 
 
-arreglar <- function(data){
-  dataT <- as.data.frame(matrix(ncol = length(data$x)))
-  dataT[1,] <- data$y
-  colnames(dataT) <- data$x
-  dataTT <- rbind(rep(max(data$y*1.15,100), length(data$x)), rep(0, length(data$x)), dataT )
-  return(dataTT)
-}
-
-tt <- arreglar(testSpider)
-tt <- as.data.frame(tt)
 
 
-tikzDevice::tikz('../ENEI2022/Graficas/prueba.tex', standAlone = F, bg = "transparent" , width = 4 , height= 2.75, sanitize= F)
-op <- par(mar = c(1, 2, 2, 2))
-fmsb::radarchart(tt, #custom polygon
-                 pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 )
-dev.off()
+
+
+
+
+
+graficaRadar(testSpider, file = 'C:/Users/hagarcia/Documents/Proyectos/ENEI2022/Graficas/prueba.tex')

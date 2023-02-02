fbfe7b0949afef91bc283b61937a425b4fe3ad4d
paulagm12 <108830435+paulagm12@users.noreply.github.com>
2023-02-02 10:08:56 -0600

    Arregla gráfica línea problema if-statement

--- R/funcionesGraficas.R
+++ R/funcionesGraficas.R
@@ -123,9 +123,9 @@ graficaLinea <- function(data, color1 = pkg.env$color1, inicio = -1, ancho = 1.5
   # print(pkg.env$modalidad)
   grafica <- ggplot2::ggplot(data, ggplot2::aes(x,y, group=1))
   grafica <- grafica + ggplot2::geom_line( colour = color1, size = ancho)+
-  if (area == T) {
-    geom_area( fill= color1, alpha=0.4) + }
     ggplot2::labs(x=NULL,y=NULL)
+  if (area == T) {
+    grafica <- grafica + geom_area( fill= color1, alpha=0.4)}
   grafica <- etiquetasLineas(grafica, calcularPosiciones(grafica), precision = pkg.env$precision)
   margenArriba <- pt2mm(calcularAlto(10))
   ## Rotanto las etiquetas del eje x cuando la modalidad es trimestral

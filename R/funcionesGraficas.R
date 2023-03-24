57103c99d2eae00e5a4c0fa47e59e409edef9f07
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-03-24 09:20:50 -0600

    prueba

--- R/funcionesGraficas.R
+++ R/funcionesGraficas.R
@@ -123,9 +123,6 @@ graficaLinea <- function(data, color1 = pkg.env$color1, inicio = -1, ancho = 1.5
   # print("La modalidad es: \n")
   # print(pkg.env$modalidad)
   grafica <- ggplot2::ggplot(data, ggplot2::aes(x,y, group=1))
-  print(3)
-  return(grafica)
-  print(2)
   grafica <- grafica + ggplot2::geom_line( colour = color1, size = ancho)+
     ggplot2::labs(x=NULL,y=NULL)
   if(etiquetaCadaSeis){

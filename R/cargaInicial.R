94159ef56cab2257cfdf0eab3292f93159311e2c
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-03 18:56:46 -0600

    Update cargaInicial.R
    quitar carga de fuentes
    quitar diccionario
    mejorar legibilidad en general

--- R/cargaInicial.R
+++ R/cargaInicial.R
@@ -1,51 +1,48 @@
 .onAttach <- function(libname, pkgname) {
+  # Cargamos la biblioteca 'extrafont' y mostramos un mensaje inicial
   require(extrafont)
   packageStartupMessage("Este paquete usa una fuente en especifico, registrandola")
-  ## Load all fonts
-  extrafont::loadfonts("pdf", quiet = TRUE)
-  extrafont::loadfonts("postscript", quiet = TRUE)
-  if (!"Open Sans Condensed Light" %in% fonts()) {
-    # print("Vamos a importar las fuentes, ingrese Y y presione ENTER")
-    font_import()
-  }
+  
+  # Si el sistema operativo es Windows, cargamos las fuentes específicas de Windows
   if (.Platform$OS.type == "windows"){
-    extrafont::loadfonts("win", quiet=TRUE)
+    extrafont::loadfonts("win", quiet = TRUE)
   }
+
+  # Establecemos las opciones para TikZ
   options(tikzDefaultEngine = "xetex")
-  #options(tikzXelatex = "/usr/local/texlive/2014/bin/x86_64-linux/xelatex")
   options(tikzXelatexPackages = c("\\usepackage[T1]{fontenc}",
-                                  "\\usepackage{tikz}\n",
-                                  "\\usepackage[active,tightpage,xetex]{preview}\n",
-                                  "\\usepackage{fontspec,xunicode}\n",
-                                  "\\PreviewEnvironment{pgfpicture}\n",
-                                  "\\setlength\\PreviewBorder{0pt}\n",
-                                  "\\usetikzlibrary{calc}\n",
-                                  "\\usetikzlibrary{positioning}\n",
-                                  "\\usepackage{fontspec,xunicode}\n",
-                                  "\\setmainfont{Open Sans Condensed Light}\n"))
-  options(tikzUnicodeMetricPackages = c("\\usetikzlibrary{calc}\n"))
-  if (.Platform$OS.type == "unix"){
-    rutaPadre <- "~/Dictionary"
-    rutaDiccionario <- "tikzMetricsDictionary"
-  }
-  else {
-    usuario <- Sys.getenv("USERNAME")
-    rutaPadre <- paste0("C:/Users/", usuario)
-    rutaDiccionario <- "tikzMetricsDictionary"
-  }
+                                  "\\usepackage{tikz}",
+                                  "\\usepackage[active,tightpage,xetex]{preview}",
+                                  "\\usepackage{fontspec,xunicode}",
+                                  "\\PreviewEnvironment{pgfpicture}",
+                                  "\\setlength\\PreviewBorder{0pt}",
+                                  "\\usetikzlibrary{calc}",
+                                  "\\usetikzlibrary{positioning}",
+                                  "\\setmainfont{Open Sans Condensed Light}"))
+  options(tikzUnicodeMetricPackages = c("\\usetikzlibrary{calc}"))
+  
+  # Establecemos la ruta del diccionario según el sistema operativo
+  rutaPadre <- ifelse(.Platform$OS.type == "unix", "~/Dictionary", file.path("C:/Users", Sys.getenv("USERNAME")))
+  rutaDiccionario <- "tikzMetricsDictionary"
+  
+  # Creamos la ruta del padre si no existe
   if (!file.exists(rutaPadre)) {
-    dir.create(file.path(rutaPadre))
+    dir.create(rutaPadre)
   }
+  
+  # Establecemos la opción 'tikzMetricsDictionary' 
   options(tikzMetricsDictionary = file.path(rutaPadre, rutaDiccionario))
-  # Variables para el tamaño de la gráfica
+  
+  # Establecemos las variables de entorno del paquete
   pkg.env$alto <- 1.91
   pkg.env$ancho <- 3.19
-  # Variables para las legendas
-  pkg.env$longCuadrado <- 2.5 #<--- Esta está en mm
+  pkg.env$longCuadrado <- 2.5
   pkg.env$tol <- pkg.env$ancho / 40
-  # La longitud máxima para una palabra
   pkg.env$longitudMaxima <- 25
-  # Verificando si existe el diccionario de palabras
-  crearDiccionarioPalabras()
+  
+  # Verificamos si existe el diccionario de palabras y, si no, lo creamos
+  # crearDiccionarioPalabras() # no se usa u.u
+  
+  # Establecemos la fuente del paquete
   pkg.env$fuente <- "Open Sans Condensed Light"
 }

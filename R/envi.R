0c34a84d405b25369d939ad94033c9e4e9d34adb
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-03 18:56:53 -0600

    Update envi.R
    mejorar legibilidad en general

--- R/envi.R
+++ R/envi.R
@@ -1,15 +1,29 @@
+# Creamos un nuevo entorno para el paquete
 pkg.env <- new.env()
-pkg.env$fEscala <- 0.85039370025172
-pkg.env$color1 <- rgb(0, 0, 0, maxColorValue = 255) #0 0 0
-pkg.env$color2 <- rgb(0.5, 0.5, 0.5)
-pkg.env$colorRelleno <- rgb(1, 1, 1) # 1 1 1
-pkg.env$repu <- c("Total República, Total republica, Total república, Total Republica")
-pkg.env$ignorado <- c("Ignorado", "ignorado", "IGNORADO", "Ignorada", "ignorada", "otro", "otros", "otra", "otras", "Otras", "Otros")
-pkg.env$exclusion <- c(pkg.env$ignorado, "otro", "otros", "otra", "otras", "Otras", "Otros")
-pkg.env$gris <- rgb(200, 200, 200, maxColorValue = 255)
+
+# Definimos colores en formato RGB
+pkg.env$color1 <- rgb(0, 0, 0, maxColorValue = 255) # Color negro
+pkg.env$color2 <- rgb(0.5, 0.5, 0.5) # Color gris
+pkg.env$colorRelleno <- rgb(1, 1, 1) # Color blanco
+pkg.env$gris <- rgb(200, 200, 200, maxColorValue = 255) 
 pkg.env$grisBase <- rgb(152, 152, 152, maxColorValue = 255)
+
+# Definimos parámetros generales
+pkg.env$fEscala <- 0.85039370025172
 pkg.env$fontSize <- 11
-pkg.env$botarCeros <- TRUE
-pkg.env$modalidad <- NULL
 pkg.env$tamEti <- 3.2
+pkg.env$botarCeros <- TRUE
 pkg.env$maxMin <- FALSE
+pkg.env$modalidad <- NULL
+
+# Definimos las repúblicas
+pkg.env$repu <- c("Total República", "Total republica", "Total república", "Total Republica")
+
+# Definimos los valores que se van a ignorar
+pkg.env$ignorado <- c("Ignorado", "ignorado", "IGNORADO", "Ignorada", "ignorada")
+
+# Definimos los valores adicionales a excluir
+exclusion_adicional <- c("otro", "otros", "otra", "otras", "Otras", "Otros")
+
+# Unimos los valores ignorados y los adicionales en una lista de exclusión
+pkg.env$exclusion <- c(pkg.env$ignorado, exclusion_adicional)
